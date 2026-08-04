e287b42d173885dcd4f72bb1c8597278727a68b6
bien <bien2k4.email@protonmail.com>
2026-08-04 14:56:23 +0700

    fix(android): buffer service IPC messages during connect

--- app-flutter/lib/src/ipc/android_service_transport.dart
+++ app-flutter/lib/src/ipc/android_service_transport.dart
@@ -5,12 +5,46 @@ import 'package:stream_channel/stream_channel.dart';
 
 import 'ipc_transport.dart';
 
+class BufferedBroadcastStream<T> {
+  final List<T> _pending = [];
+
+  late final StreamController<T> _controller =
+      StreamController<T>.broadcast(onListen: _flush);
+
+  Stream<T> get stream => _controller.stream;
+
+  void add(T value) {
+    if (_controller.hasListener) {
+      _controller.add(value);
+    } else {
+      _pending.add(value);
+    }
+  }
+
+  void addError(Object error, [StackTrace? stackTrace]) {
+    _controller.addError(error, stackTrace);
+  }
+
+  Future<void> close() async {
+    _pending.clear();
+    await _controller.close();
+  }
+
+  void _flush() {
+    final pending = List<T>.of(_pending);
+    _pending.clear();
+    for (final value in pending) {
+      _controller.add(value);
+    }
+  }
+}
+
 class AndroidServiceTransport implements IpcTransport {
   static const _controlChannel = MethodChannel('rift/android/service_rpc');
   static const _eventChannel = EventChannel('rift/android/service_rpc_events');
 
   StreamSubscription<dynamic>? _eventSubscription;
-  StreamController<String>? _incoming;
+  BufferedBroadcastStream<String>? _incoming;
   StreamController<String>? _outgoing;
   bool _connected = false;
 
@@ -20,7 +54,7 @@ class AndroidServiceTransport implements IpcTransport {
       return StreamChannel<String>(_incoming!.stream, _outgoing!.sink);
     }
 
-    final incoming = StreamController<String>.broadcast();
+    final incoming = BufferedBroadcastStream<String>();
     final outgoing = StreamController<String>();
     _incoming = incoming;
     _outgoing = outgoing;
